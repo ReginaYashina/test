c21dc497234adf267e5506329b8f0cc9621d41c4
geekhouse <m.v.sheleg@gmail.com>
2019-01-12 11:38:50 +0300

    parser 2

--- app/Parser/Aliexpress.php
+++ app/Parser/Aliexpress.php
@@ -3,6 +3,7 @@ namespace App\Parser;
 
 use Symfony\Component\DomCrawler\Crawler;
 use Auth;
+use App\Product;
 
 class Aliexpress implements ParseContract
 {
@@ -15,16 +16,29 @@ class Aliexpress implements ParseContract
         header('Content-Type: text/html; charset=utf-8');
     }
 
-    public function getParse($url = null)
+    public function getParse($url = null, $id=null)
     {
      
-        $file = file_get_contents($url);//весь html страницы, которую парсим
+        $file = file_get_contents($url);
         $this->crawler = new Crawler($file);
-		$body=$this->crawler->filter('body')->html();
-        // $('$body').each(function(){
-        //     var pct=$(this).filter('.item')
-        // });
-        // echo pct;
-        echo $body;
-    }
+		$body=$this->crawler->filter('.dtList')->each(function(Crawler $node, $i){
+		$name = $node->text();
+		$pic = $node->filter('img')->attr('src');
+		$obj = new Product;
+		$obj->name = $name;
+		$obj->picture = $pic;
+		$arr_pic = explode('/', $pic);
+		$newfile = $_SERVER['DOCUMENT_ROOT'].'/uploads/thumb/'.end($arr_pic);
+			if (!copy('http:'.$pic, $newfile)) {
+			echo "не удалось скопировать $file...\n";
+			}
+		$obj->save();
+		echo $name;
+		echo '<img src = "http:'.$pic.'">';
+		echo '<hr/>';
+	});}
 }
+       
+	
+
+
